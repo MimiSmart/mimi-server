123eeb9bd1ecdbb23cc0f0346c8c5d600a3604e7
arsyaCRK <arsyacrk@gmail.com>
2023-07-03 16:54:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,21 +12,6 @@ else
     exit 1
 fi
 
-# Очищаем таблицу IPTABLES
-read -p "Вы хотите очистить таблицу IPTABLES? (y/n): " choice
-if [[ $choice == "y" || $choice == "Y" ]]; then
-    iptables -P INPUT ACCEPT
-    iptables -P FORWARD ACCEPT
-    iptables -P OUTPUT ACCEPT
-    iptables -t nat -F
-    iptables -t mangle -F
-    iptables -F
-    iptables -X
-else
-    echo "Хорошо. Не буду."
-fi
-
-
 
 # Активируем необходимые модули
 echo "Активирую необходимые модули Apache"
